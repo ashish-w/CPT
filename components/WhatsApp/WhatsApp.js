23e19ce5490646f76cd2633a091191a49a05656e
Deepansh Dubey <56029566+deepanshdubey@users.noreply.github.com>
2023-08-08 21:18:39 +0530

    Initial Commit
    Starting

---
+++ components/WhatsApp/WhatsApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WhatsApp =  () => {
+  return (
+    <div className=''>
+      <a href="https://api.whatsapp.com/send?phone=918130263311" target='_blank'>
+        <img className='whatsAppButton' src="whatsAppButton.svg" alt="WhatsAppButton"/>
+      </a>
+    </div>
+  )
+}
+
+export default WhatsApp
