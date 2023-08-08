dd60a440c5d5921b95be799b045a8f1079fad6bb
Deepansh Dubey <56029566+deepanshdubey@users.noreply.github.com>
2023-08-09 02:04:54 +0530

    Tara changed, Achha wala chle gya ab

--- components/ReserveForms/SummaryAndCheckout/SummaryAndCheckout.js
+++ components/ReserveForms/SummaryAndCheckout/SummaryAndCheckout.js
@@ -8,7 +8,6 @@ import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import styles from "../../../styles/bookform.module.css";
-import star from '../../../public/star.png'
 
 const SummaryAndCheckout = (props) => {
   const { count, setCount, paymentStatus, setPaymentStatus, tourData } = props;
@@ -280,9 +279,24 @@ const SummaryAndCheckout = (props) => {
                       className="col-6 d-flex"
                       style={{
                         justifyContent: "right",
+                        alignItems: "center"
                       }}
                     >
-                     <img src="starLogo.svg" height="20px" width="20px"  alt="rating" /> <div style={{paddingRight : "10px"}}>  </div> {  tourData.rating}
+                      <svg
+                        viewBox="64 64 896 896"
+                        focusable="false"
+                        data-icon="star"
+                        width="1em"
+                        height="1em"
+                        fill="currentColor"
+                        aria-hidden="true"
+                        color="#f7e64f"
+                      >
+                        <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+                      </svg>
+                      <div style={{ paddingRight: "10px" }}>
+                      </div>
+                        {tourData.rating}
                     </div>
                   </div>
 
@@ -293,12 +307,13 @@ const SummaryAndCheckout = (props) => {
                         fontWeight: 700,
                       }}
                     >
-                      Date: {`${new Date(count.tourDate).toLocaleString("en-US", {
+                      Date:{" "}
+                      {`${new Date(count.tourDate).toLocaleString("en-US", {
                         month: "short",
                         day: "2-digit",
                         year: "numeric",
                         weekday: "short",
-                        })}`}
+                      })}`}
                     </div>
                     <div
                       className="col-6 d-flex"
@@ -307,7 +322,8 @@ const SummaryAndCheckout = (props) => {
                         fontWeight: 700,
                       }}
                     >
-                      Time: {`${new Date(count.tourDate).toLocaleString("en-US", {
+                      Time:{" "}
+                      {`${new Date(count.tourDate).toLocaleString("en-US", {
                         hour: "2-digit",
                         hour12: false,
                         minute: "2-digit",
@@ -606,20 +622,18 @@ const SummaryAndCheckout = (props) => {
               />
             </div>
 
-
             <div className="checkout-section text-right pt-3">
-          <LazyLoadImage
-            className="mr-4"
-            width="100"
-            alt="masterCard"
-            src="/images/visamastercard.webp"
-          />
+              <LazyLoadImage
+                className="mr-4"
+                width="100"
+                alt="masterCard"
+                src="/images/visamastercard.webp"
+              />
 
-          <button type="submit" className="btn-reserve text-uppercase">
-            Finish Order
-          </button>
-        </div>
-
+              <button type="submit" className="btn-reserve text-uppercase">
+                Finish Order
+              </button>
+            </div>
           </div>
 
           {/* <div className="col-xs-12 col-lg-6">
@@ -711,7 +725,6 @@ const SummaryAndCheckout = (props) => {
             <p className="error">{error}</p>
                 </div> */}
         </div>
-        
       </form>
     </div>
   );
